79d17cd32dcd01d4840079860900bb0b461b42ff
Savannah Dockerty <105252792+sdock0@users.noreply.github.com>
2023-10-03 15:46:34 +1000

    Update VisitNoteRepository.java

--- src/main/java/com/optimed/patientmicroservice/repository/VisitNoteRepository.java
+++ src/main/java/com/optimed/patientmicroservice/repository/VisitNoteRepository.java
@@ -1,10 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.patientservice.repository;
 
-import com.patientservice.model.VisitNote;
+package com.optimed.patientmicroservice.repository;
+
+import com.optimed.patientmicroservice.model.VisitNote;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
