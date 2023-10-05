510f89aa6cd494f930ab8c7ae207ee008cb5643e
Savannah Dockerty <105252792+sdock0@users.noreply.github.com>
2023-10-05 19:06:39 +1000

    Update ConditionRepository.java

--- src/main/java/com/optimed/patientmicroservice/repository/ConditionRepository.java
+++ src/main/java/com/optimed/patientmicroservice/repository/ConditionRepository.java
@@ -2,6 +2,7 @@
 package com.optimed.patientmicroservice.repository;
 
 import com.optimed.patientmicroservice.model.Condition;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
