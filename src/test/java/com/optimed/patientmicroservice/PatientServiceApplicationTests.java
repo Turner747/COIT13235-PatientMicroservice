7da758a42e92c0003e9306ee8942238a9a5ea3f9
Savannah Dockerty <105252792+sdock0@users.noreply.github.com>
2023-10-05 18:51:20 +1000

    Add files via upload

---
+++ src/test/java/com/optimed/patientmicroservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimed.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
