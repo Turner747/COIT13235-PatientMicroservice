ca34d3d8ce47ec1e7e7126cbf34649ee50954dd5
Savannah Dockerty <105252792+sdock0@users.noreply.github.com>
2023-10-13 11:40:32 +1000

    Add files via upload

--- src/main/java/com/optimed/patientmicroservice/repository/ConditionRepository.java
+++ src/main/java/com/optimed/patientmicroservice/repository/ConditionRepository.java
@@ -1,12 +1,13 @@
 
 package com.optimed.patientmicroservice.repository;
-
 import com.optimed.patientmicroservice.model.Condition;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ConditionRepository extends JpaRepository<Condition, Long> {
-
+    
 }
--- src/main/java/com/optimed/patientmicroservice/repository/PatientRepository.java
+++ src/main/java/com/optimed/patientmicroservice/repository/PatientRepository.java
@@ -3,10 +3,13 @@ package com.optimed.patientmicroservice.repository;
 
 import com.optimed.patientmicroservice.model.Patient;
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
-
+    
 }
