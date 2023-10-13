7ca8ac0222984c0135518d92cf59db7b31c444c9
kunshrestha <kunshrestha@hotmail.com>
2023-10-13 16:14:48 +1100

    @EnableDiscoveryClient

--- src/main/java/com/optimed/patientmicroservice/PatientMicroserviceApplication.java
+++ src/main/java/com/optimed/patientmicroservice/PatientMicroserviceApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableAutoConfiguration
 @EnableJpaRepositories(basePackages = "com.optimed.patientmicroservice.repository")
 @EntityScan(basePackages = "com.optimed.patientmicroservice.model")
