44a8f54dbdd0b1698e8c47f454a94d64b1082e65
kunshrestha <kunshrestha@hotmail.com>
2023-10-13 16:19:42 +1100

    AppointmentResponse class fix

--- src/main/java/com/optimed/patientmicroservice/response/AppointmentResponse.java
+++ src/main/java/com/optimed/patientmicroservice/response/AppointmentResponse.java
@@ -1,8 +1,11 @@
 
 package com.optimed.patientmicroservice.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.Date;
 
 @Data
@@ -11,21 +14,23 @@ public class AppointmentResponse {
     
     @Column(name = "appointment_id")
     private Long id;
-    
+    @JsonProperty("patient")
     private PatientResponse patient;
-    
+    @JsonProperty("doctor")
     private StaffResponse doctor;
     
     @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     @Column(name = "appointment_start_time")
     private Date start_time;
     
     @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     @Column(name = "appointment_end_time")
     private Date end_time;
     
-    @Enumerated(EnumType.STRING)
+//    @Enumerated(EnumType.STRING)
     @Column(name = "appointment_status")
-    private AppointmentResponse status;
-    
+//    private AppointmentResponse status;
+    private String status;
 }
