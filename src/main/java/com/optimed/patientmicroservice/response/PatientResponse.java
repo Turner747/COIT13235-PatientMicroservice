7f2040330246d400715886b3c0f3aef65467d738
Savannah Dockerty <105252792+sdock0@users.noreply.github.com>
2023-10-14 11:07:31 +1000

    Update PatientResponse.java

--- src/main/java/com/optimed/patientmicroservice/response/PatientResponse.java
+++ src/main/java/com/optimed/patientmicroservice/response/PatientResponse.java
@@ -1,30 +1,25 @@
-
 package com.optimed.patientmicroservice.response;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import java.util.Date;
 import lombok.Data;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedDate;
 
 @Data
 @Embeddable
 public class PatientResponse {
     
     private Long id;
-    private String first_name;
+    private String firstName;
     private String surname;
     
     @Temporal(TemporalType.DATE)
-    private Date date_of_birth;
+    private Date dateOfBirth;
     
-    private String phone_number;
+    private String phoneNbr;
     
-    private String email_address;
+    private String emailAddress;
     
     private String street;
     
@@ -33,4 +28,5 @@ public class PatientResponse {
     private String state;
     
     private String postcode;
+        
 }
