2d1be595fa7b4c85c9a7902384b765167a981978
kunshrestha <kunshrestha@hotmail.com>
2023-10-16 18:51:37 +1100

    Fix missing doctor information

--- src/main/java/com/optimed/patientmicroservice/controller/ConditionController.java
+++ src/main/java/com/optimed/patientmicroservice/controller/ConditionController.java
@@ -1,6 +1,7 @@
 
 package com.optimed.patientmicroservice.controller;
 
+import com.optimed.patientmicroservice.client.StaffClient;
 import com.optimed.patientmicroservice.mapper.ObjectMapper;
 import com.optimed.patientmicroservice.model.Condition;
 import com.optimed.patientmicroservice.response.ConditionResponse;
@@ -8,7 +9,10 @@ import com.optimed.patientmicroservice.response.ConditionResponse;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+
+import com.optimed.patientmicroservice.response.StaffResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,9 +24,12 @@ import com.optimed.patientmicroservice.repository.ConditionRepository;
 public class ConditionController {
     
     private ConditionRepository conditionRepo;
+    @Autowired
+    private StaffClient staffClient;
     
     @PostMapping(consumes = "application/json")
     public ResponseEntity<ConditionResponse> saveCondition(@RequestBody ConditionResponse conditionResponse) {
+        conditionResponse.setDoctor(ObjectMapper.map(staffClient.getStaffByID(conditionResponse.getDoctor().getId()).getBody(), StaffResponse.class));
         Condition new_condition = conditionRepo.save(ObjectMapper.map(conditionResponse, Condition.class));
         return ResponseEntity.status(HttpStatus.CREATED).body(ObjectMapper.map(new_condition, ConditionResponse.class));
     }
